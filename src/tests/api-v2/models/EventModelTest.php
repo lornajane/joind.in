9bd0b1c10d29890d72e1fc7630731ba9a33facd1
Lorna <lorna@lornajane.net>
2011-09-10 22:28:11 +0100

    updating tests after API refactor, they run, but fail

--- src/tests/api-v2/models/EventModelTest.php
+++ src/tests/api-v2/models/EventModelTest.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Event model test
+ * Event Mapper test
  *
  * PHP version 5
  *
- * @category Model
+ * @category Mapper
  * @package  APIv2_Tests
  * @author   Rob Allen <rob@akrabat.com>
  * @license  BSD see doc/LICENSE
@@ -12,15 +12,15 @@
  */
 
 /**
- * Event model test
+ * Event Mapper test
  *
- * @category Model
+ * @category Mapper
  * @package  APIv2_Tests
  * @author   Rob Allen <rob@akrabat.com>
  * @license  BSD see doc/LICENSE
  * @link     http://github.com/joindin/joind.in
  */
-class Model_EventModelTest extends PHPUnit_Framework_TestCase
+class Mapper_EventMapperTest extends PHPUnit_Framework_TestCase
 {
     /**
      * Set up
@@ -40,10 +40,11 @@ class Model_EventModelTest extends PHPUnit_Framework_TestCase
     public function testGetAllEventsReturnsAList()
     {
         $db = getDbAdapater();
+        $mapper = new EventMapper($db, new stdClass());
         $resultsperpage = 20;
         $start = 0;
         $verbose = 0;
-        $list = EventModel::getEventList($db, $resultsperpage, $start, $verbose);
+        $list = $mapper->getEventList($resultsperpage, $start, $verbose);
         
         $this->assertGreaterThanOrEqual(2, count($list));
         $this->assertEquals('1', $list[count($list)-1]['event_id']);
@@ -57,13 +58,14 @@ class Model_EventModelTest extends PHPUnit_Framework_TestCase
     public function testGetAllEventsPages1And2AreDifferent()
     {
         $db = getDbAdapater();
+        $mapper = new EventMapper($db, new stdClass());
         $resultsperpage = 1;
         $start = 0;
         $verbose = 0;
-        $page1 = EventModel::getEventList($db, $resultsperpage, $start, $verbose);
+        $page1 = $mapper->getEventList($db, $resultsperpage, $start, $verbose);
         
         $start = 1;
-        $page2 = EventModel::getEventList($db, $resultsperpage, $start, $verbose);
+        $page2 = $mapper->getEventList($db, $resultsperpage, $start, $verbose);
         
         $this->assertEquals('1', count($page1));
         $this->assertNotEquals($page1[0], $page2[0]);
@@ -77,13 +79,14 @@ class Model_EventModelTest extends PHPUnit_Framework_TestCase
     public function testGetAllEventsVerbose()
     {
         $db = getDbAdapater();
+        $mapper = new EventMapper($db, new stdClass());
         $resultsperpage = 1;
         $start = 0;
         $verbose = 0;
-        $simple = EventModel::getEventList($db, $resultsperpage, $start, $verbose);
+        $simple = EventMapper::getEventList($db, $resultsperpage, $start, $verbose);
        
         $verbose = 1;
-        $verbose = EventModel::getEventList($db, $resultsperpage, $start, $verbose);
+        $verbose = EventMapper::getEventList($db, $resultsperpage, $start, $verbose);
         $this->assertTrue(array_key_exists('tz_continent', $verbose[0]));
         $this->assertNotEquals($simple[0], $verbose[0]);
     }
